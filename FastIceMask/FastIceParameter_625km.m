b88fae335bfd469de9cc07cb5c92eb6ac45526b9
Mou-si <70570276+Mou-si@users.noreply.github.com>
2024-05-30 16:09:33 +0900

    update v1.0.0
    - improve the readme.md
    - fix the bugs in saving results
    - update the evaluate codes
    - update the NameList used to create the dataset

---
+++ FastIceMask/FastIceParameter_625km.m
@@ -0,0 +1,17 @@
+function FastIceFlag = FastIceParameter
+% The function is used to set the parameter of Land Fast Ice Mask mode.
+% Now this functio is applied to the Fraser land fast-ice mask. You can 
+% download it here (doi:10.26179/5d267d1ceb60c) and some pretreatment is
+% needed.
+
+global FastIce
+
+FastIce.TimeCover = datetime('2000-01-01') : datetime('2018-02-28');
+FastIce.Dir = 'G:\FraserLandFastIce\nc_12500\';
+FastIce.Name1 = '';
+FastIce.Name2 = '.nc';
+FastIce.VarName = 'LandFastIce';
+
+FastIce.TimeCover = [min(datenum(FastIce.TimeCover)), max(datenum(FastIce.TimeCover))];
+FastIceFlag = true;
+end
